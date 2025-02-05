377102fdb871d02117a0258e33c8a67e1f8ec3a0
david <david.crei@castgroup.com.br>
2025-02-05 17:08:36 -0300

    ajustes

---
+++ infrastructure/src/main/java/com/dcvideo/admin/catalogo/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.dcvideo.admin.catalogo.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.dcvideo.admin.catalogo")
+public class WebServerConfig {
+}
